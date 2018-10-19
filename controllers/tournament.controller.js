2cf36ae36164093abbf16680e838657a143da4d2
GlennV10 <verlindenglenn@hotmail.com>
2018-10-20 00:41:22 +0200

    Tournament routes

--- controllers/tournament.controller.js
+++ controllers/tournament.controller.js
@@ -10,24 +10,37 @@ const Tournament = require('../models/Tournament');
 /* === GET === */
 exports.getAllTournaments = (req, res, next) => {
    Tournament.find({})
-   .sort({ time: 1 })
-   .then((tournaments) => {    
-      res.json(tournaments);
-   })
-   .catch(next);
+      .sort({ time: 1 })
+      .then((tournaments) => {
+         res.json(tournaments);
+      })
+      .catch(next);
 };
 
-exports.getStartingTournaments = (req, res, next) => {
+exports.getStartingTournaments = async (req, res, next) => {
    let hour = moment().hour();
    let minute = moment().minute();
 
    Tournament.find({ "time.hour": { $gte: hour } })
    .sort({ time: 1 })
    .then((tournaments) => {
-      let startingTournaments = tournaments.filter(e => e.time.hour === hour ? e.time.minute >= minute : true);
-      if (startingTournaments.length > 4) startingTournaments.length = 4;
+      let startingTournaments = tournaments.filter(e => e.time.hour === hour ? e.time.minute > minute : true);
 
-      res.json(startingTournaments);
+      if (startingTournaments.length > 4) {
+         startingTournaments.length = 4;
+      } else {
+         Tournament.find({})
+         .sort({ time: 1 })
+         .limit(4 - startingTournaments.length)
+         .then((tournaments) => {
+            startingTournaments = startingTournaments.concat(tournaments);
+         });
+      }
+
+      getRemainingTime(startingTournaments, minute)
+      .then(tournaments => {
+         res.json(tournaments);
+      });
    })
    .catch(next);
 };
@@ -49,7 +62,7 @@ exports.updateTournament = (req, res, next) => {
          _id: req.params.id
       })
       .then((tournament) => {
-         if (!tournament) res.json({ success: false, message: 'No tournament found.'});
+         if (!tournament) res.json({ success: false, message: 'No tournament found.' });
          if (tournament) res.json({ success: true, message: 'Tournament updated.', tournament });
       })
       .catch(next);
@@ -62,9 +75,34 @@ exports.deleteTournament = (req, res, next) => {
    Tournament.findOneAndDelete({
       _id: req.params.id
    })
-   .then((tournament) => {
-      if (!tournament) res.json({ success: false, message: 'No tournament found.' })
-      if (tournament) res.json({ success: true, message: 'Tournament deleted.', tournament });
-   })
-   .catch(next);
+      .then((tournament) => {
+         if (!tournament) res.json({ success: false, message: 'No tournament found.' })
+         if (tournament) res.json({ success: true, message: 'Tournament deleted.', tournament });
+      })
+      .catch(next);
 };
+
+/** 
+ * Helper functions
+ */
+getRemainingTime = async (tournaments, time) => {
+   return new Promise((resolve, reject) => {
+      if (tournaments) {
+         let startingTournaments = [];
+         for (let tournament of tournaments) {
+            newTournament = { tournament, startsIn: 0 };
+   
+            if (tournament.time.minute > time) {
+               newTournament.startsIn = tournament.time.minute - time;
+            } else {
+               newTournament.startsIn = (60 - time) + tournament.time.minute;
+            };
+
+            startingTournaments.push(newTournament);
+         }
+         resolve(startingTournaments);
+      } else {
+         reject('No tournaments.');
+      }
+   });
+}; 
